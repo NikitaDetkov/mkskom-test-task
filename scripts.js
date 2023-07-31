5b37fc36ab884d7b2f7a4d43b3032da159fc888c
Nikita Detkov <vasyanikita55@gmail.com>
2023-07-31 16:20:48 +0300

    fifth commit

--- scripts.js
+++ scripts.js
@@ -3,34 +3,28 @@ const App = {
         albumId: '',
         images_buff: [],
         images: [],
+        isFlex: true,
         list_folders: [
             {
-                name: 'Designer',
-                num: 23
+                name: 'Designer', num: 23
             },
             {
-                name: 'Code Blocks',
-                num: 15
+                name: 'Code Blocks', num: 15
             },
             {
-                name: 'Drafts',
-                num: 18
+                name: 'Drafts', num: 18
             },
             {
-                name: 'Peoples Review',
-                num: 21
+                name: 'Peoples Review', num: 21
             },
             {
-                name: 'Social',
-                num: 43
+                name: 'Social', num: 43
             },
             {
-                name: 'Labels',
-                num: 36
+                name: 'Labels', num: 36
             },
             {
-                name: 'My Notes',
-                num: 29
+                name: 'My Notes', num: 29
             },
         ]
     }),
@@ -39,25 +33,33 @@ const App = {
             const url = `https://jsonplaceholder.typicode.com/albums/${albumId}/photos`;
             let response = await fetch(url);
             this.images_buff = await response.json();
+
             this.images = this.images_buff.slice(0, 16);
-            albumId = '';
+            this.top_images =  this.images.slice(0, 2);
+            this.bottom_images =  this.images.slice(2, 5);
         },
         onlyNumbers(input) {
             let regChars = /[\D]/g;
             input.target.value = input.target.value.replace(regChars, '');
         },
         toFlex() {
-            let container = document.querySelector('#images-container');
-            container.classList.remove('images-container-grid');
-            container.classList.add('images-container');
-            this.images = this.images_buff.slice(0, 16);
+            let container_flex = document.querySelector('#images-container');
+            let container_grid = document.querySelector('#images-container-grid');
+
+            container_flex.classList.remove('d-none');
+            container_grid.classList.add('d-none');
+
+            this.isFlex = true;
         },
         toGrid() {
-            let container = document.querySelector('#images-container');
-            container.classList.remove('images-container');
-            container.classList.add('images-container-grid');
-            this.images = this.images_buff.slice(0, 5);
-        }
+            let container_flex = document.querySelector('#images-container');
+            let container_grid = document.querySelector('#images-container-grid');
+
+            container_flex.classList.add('d-none');
+            container_grid.classList.remove('d-none');
+
+            this.isFlex = false;
+        },
     },
     mounted() {
         this.getImages(1)
